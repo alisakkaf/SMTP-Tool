ff5dcc70d57d4c747382e3ba78d0d44b962aeb60
alisakkaf <support@alisakkaf.com>
2026-09-07 01:03:34 +0300

    Update SingleSessionHandler_Abstract.cs file

--- source/SMTPtool/SessionTab/SingleSessionHandler_Abstract.cs
+++ source/SMTPtool/SessionTab/SingleSessionHandler_Abstract.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SMTPtestTool
+namespace SMTPtool
 {
     public abstract class SingleSessionHandler_Abstract
     {
