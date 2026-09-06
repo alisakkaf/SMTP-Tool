81b9b8cfb1cd041f76e6471b6056031f006fc8e2
alisakkaf <support@alisakkaf.com>
2026-09-07 01:03:32 +0300

    Update AssemblyInfo.cs file

--- source/SMTPtool/Properties/AssemblyInfo.cs
+++ source/SMTPtool/Properties/AssemblyInfo.cs
@@ -1,36 +1,19 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("SMTP Test Tool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SMTP Tool")]
+[assembly: AssemblyDescription("Modern SMTP Testing, Diagnostics & Debugging Suite for Windows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Georg Felgitsch")]
-[assembly: AssemblyProduct("SMTP Test Tool")]
-[assembly: AssemblyCopyright("Georg Felgitsch, 2015 - 2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("AliSakkaF")]
+[assembly: AssemblyProduct("SMTP Tool")]
+[assembly: AssemblyCopyright("Copyright © 2026 Ali Sakkaf (https://alisakkaf.com)")]
+[assembly: AssemblyTrademark("SMTP Tool")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
 [assembly: ComVisible(false)]
 
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("84d1d9c2-36d5-448f-aec4-b9139d926f82")]
+[assembly: Guid("f0dded66-6e54-40d7-9f57-91455757bec1")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0")]
